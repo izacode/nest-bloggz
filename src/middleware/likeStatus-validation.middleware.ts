7ee9ad973f8d069707a12f08e6cb0b62e0fbbc95
izacode <izaelec@gmail.com>
2022-08-28 19:42:10 -0400

    typeof req.body.likeStatus string check added to likestatus middleware

--- src/middleware/likeStatus-validation.middleware.ts
+++ src/middleware/likeStatus-validation.middleware.ts
@@ -5,15 +5,10 @@ import { Request, Response, NextFunction } from 'express';
 export class LikeStatusValidationMiddleware implements NestMiddleware {
   // @InjectModel(Attempt.name) private attemptModel: Model<Attempt>;
   async use(req: Request, res: Response, next: NextFunction) {
-    console.log('inside LikeStatusValidationMiddleware');
-    console.log("path------", req.url)
-    console.log("likeStatus --------",req.body.likeStatus);
-    if (req.body.likeStatus) {
-      console.log('inside LikeStatusValidationMiddleware iffff');
+    if (typeof req.body.likeStatus === 'string') {
       const likeStatus: string = req.body.likeStatus;
       let errors: any;
-      if (likeStatus.length > 20 || likeStatus.length < 1) {
-        console.log('inside LikeStatusValidationMiddleware iffff2');
+      if (likeStatus.length > 20 || likeStatus === '') {
         errors = {
           errorsMessages: [
             {
