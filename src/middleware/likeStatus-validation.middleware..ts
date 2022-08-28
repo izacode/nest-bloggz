447322d3a2c9e7e4f3e463d22462938f9dfd5b4d
izacode <izaelec@gmail.com>
2022-08-28 17:00:00 -0400

    req.bodylikestatus added to likestatus middleware

--- src/middleware/likeStatus-validation.middleware..ts
+++ src/middleware/likeStatus-validation.middleware..ts
@@ -5,19 +5,22 @@ import { Request, Response, NextFunction } from 'express';
 export class LikeStatusValidationMiddleware implements NestMiddleware {
   // @InjectModel(Attempt.name) private attemptModel: Model<Attempt>;
   async use(req: Request, res: Response, next: NextFunction) {
-    const likeStatus: string = req.body.likeStatus;
-    let errors:any;
-    if (likeStatus.length > 21 || likeStatus.length < 1) {
-      errors = {
-        errorsMessages: [
-          {
-            message: 'wrong likestatus',
-            field: 'likeStatus',
-          },
-        ],
-      };
-          return res.status(400).json(errors);
+    if (req.body.likeStatus) {
+      const likeStatus: string = req.body.likeStatus;
+      let errors: any;
+      if (likeStatus.length > 21 || likeStatus.length < 1) {
+        errors = {
+          errorsMessages: [
+            {
+              message: 'wrong likestatus',
+              field: 'likeStatus',
+            },
+          ],
+        };
+        return res.status(400).json(errors);
+      }
     }
+
     next();
   }
 }
