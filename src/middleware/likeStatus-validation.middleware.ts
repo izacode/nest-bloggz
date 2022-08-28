b0c7584869307c73f7d667bb634921ed987107b9
izacode <izaelec@gmail.com>
2022-08-28 19:23:59 -0400

    more logs added to likestatus middleware

--- src/middleware/likeStatus-validation.middleware.ts
+++ src/middleware/likeStatus-validation.middleware.ts
@@ -6,11 +6,13 @@ export class LikeStatusValidationMiddleware implements NestMiddleware {
   // @InjectModel(Attempt.name) private attemptModel: Model<Attempt>;
   async use(req: Request, res: Response, next: NextFunction) {
     console.log('inside LikeStatusValidationMiddleware');
+    console.log("path------", req.url)
+    console.log("likeStatus --------",req.body.likeStatus);
     if (req.body.likeStatus) {
       console.log('inside LikeStatusValidationMiddleware iffff');
       const likeStatus: string = req.body.likeStatus;
       let errors: any;
-      if (likeStatus.length > 21 || likeStatus.length < 1) {
+      if (likeStatus.length > 20 || likeStatus.length < 1) {
         console.log('inside LikeStatusValidationMiddleware iffff2');
         errors = {
           errorsMessages: [
