4dd134bc78a80236916f5d46c54c077c631d2e21
izacode <izaelec@gmail.com>
2022-08-28 18:34:29 -0400

    request method put added to middleware in post and comments module logs added

--- src/middleware/likeStatus-validation.middleware.ts
+++ src/middleware/likeStatus-validation.middleware.ts
@@ -5,10 +5,13 @@ import { Request, Response, NextFunction } from 'express';
 export class LikeStatusValidationMiddleware implements NestMiddleware {
   // @InjectModel(Attempt.name) private attemptModel: Model<Attempt>;
   async use(req: Request, res: Response, next: NextFunction) {
+    console.log('inside LikeStatusValidationMiddleware');
     if (req.body.likeStatus) {
+      console.log('inside LikeStatusValidationMiddleware iffff');
       const likeStatus: string = req.body.likeStatus;
       let errors: any;
       if (likeStatus.length > 21 || likeStatus.length < 1) {
+        console.log('inside LikeStatusValidationMiddleware iffff2');
         errors = {
           errorsMessages: [
             {
