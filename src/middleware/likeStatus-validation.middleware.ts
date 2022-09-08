e1468802e91b4c6212cc1daae0ef2d7139529320
izacode <izaelec@gmail.com>
2022-09-08 15:17:09 -0400

    check for req headers auth admin querty case added to likestatus middleware

--- src/middleware/likeStatus-validation.middleware.ts
+++ src/middleware/likeStatus-validation.middleware.ts
@@ -13,12 +13,16 @@ export class LikeStatusValidationMiddleware implements NestMiddleware {
     );
 
     console.log('headers.auth-----', req.headers);
-  
+
     console.log('body-----', req.body);
     console.log(
       '====================================================================================================',
     );
-    if (!req.headers.hasOwnProperty('authorization') && Object.keys(req.body).length === 0)
+    if (
+      (!req.headers.hasOwnProperty('authorization') ||
+        req.headers.authorization === 'Basic admin:qwerty') &&
+      Object.keys(req.body).length === 0
+    )
       throw new UnauthorizedException();
     const statuses = ['Like', 'Dislike', 'None'];
     if (!statuses.includes(req.body.likeStatus)) {
